f02631c080df388d7722db28ca65ada2f177e441
CodeWithAtwaa <mohamedatwaa341@gmail.com>
2026-07-01 23:03:33 +0200

    Blade template and Blade Directive is done

---
+++ Laravel/example-app/resources/views/about.blade.php
@@ -0,0 +1,7 @@
+<x-layouts title="About Us">
+    <h1>{{ $title }}</h1>
+    <x-card class="mt-4 bg-gray-100 shadow-md rounded-lg p-6" style="background-color: red; border-radius: 8px; padding: 20px; color: white;" >
+        <p>This is the about page of our application. Here you can find information about our mission, vision, and team.
+        </p>
+    </x-card>
+</x-layouts>
---
+++ Laravel/example-app/resources/views/components/card.blade.php
@@ -0,0 +1,3 @@
+<div {{ $attributes->merge(['class' => 'bg-gray-100 shadow-md rounded-lg p-6']) }}>
+    {{ $slot }}
+</div>
